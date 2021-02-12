f7deb933d8052e7a22de97d8894e9a3eb7290cb2
JoelForamitti <57440945+JoelForamitti@users.noreply.github.com>
2021-02-12 15:04:45 +0100

    [docs] Fix jshtml control

--- docs/conf.py
+++ docs/conf.py
@@ -90,3 +90,4 @@ html_theme = 'sphinx_rtd_theme'  # 'alabaster'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+html_css_files = ['css/custom.css']
