e6eabc0a8dc21bd4c0a7dabae48daa22afd2a46a
JoelForamitti <57440945+JoelForamitti@users.noreply.github.com>
2021-05-27 12:34:59 +0200

    Changed random to randomize

--- tests/test_experiment.py
+++ tests/test_experiment.py
@@ -83,7 +83,7 @@ def test_random():
             self.report('x', self.model.random.random())
 
     sample = ap.Sample(parameters)
-    exp = ap.Experiment(Model, sample, iterations=2, random=False)
+    exp = ap.Experiment(Model, sample, iterations=2, randomize=False)
     results = exp.run()
 
     l = list(results.reporters['x'])
